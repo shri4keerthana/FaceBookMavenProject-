d10fe676c53e4a13359b81c11bc25990bf7f34f0
shri4keerthana <shri4keerthana@gmail.com>
2024-04-14 07:43:05 +0530

    welcome

---
+++ src/test/java/BrowserLaunch.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class BrowserLaunch {
+public static void main(String[] args) {
+	WebDriver driver= new ChromeDriver();
+	driver.get("https://adactinhotelapp.com/");
+	driver.manage().window().maximize();
+}
+}
